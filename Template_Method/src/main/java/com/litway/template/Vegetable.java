36ffcb301dd49345f996461326622fc9ac1827ac
Litway_ <xmw0517@gmail.com>
2022-09-08 09:44:51 +0800

    master  抽象类 定义了算法执行的骨架

---
+++ Template_Method/src/main/java/com/litway/template/Vegetable.java
@@ -0,0 +1,38 @@
+package com.litway.template;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class Vegetable {
+
+
+    public void cookvegetable() {
+        log.info("炒菜的一般做法...");
+        this.pourOil();
+        this.headOil();
+        this.pourVegetable();
+        this.cooking();
+    }
+
+
+    // 1- 倒油
+    public void pourOil() {
+        log.info("倒油...");
+    }
+
+    // 2- 把油烧热
+    public void headOil() {
+        log.info("加热, 把油烧热...");
+    }
+
+    // 3- 油热了之后, 将蔬菜加入锅中. 具体是哪种蔬菜由子类决定
+    public abstract void pourVegetable();
+
+    // 4- 开始炒菜
+    public void cooking() {
+        log.info("炒菜...");
+    }
+
+
+}

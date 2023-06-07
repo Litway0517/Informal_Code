08c645ec1fa6fc65a193f625fcd579a9d338dfcd
Litway_ <xmw0517@gmail.com>
2023-06-07 20:30:45 +0800

    http  更新注释

--- http/src/test/java/com/demo/litway/http/HttpTest.java
+++ http/src/test/java/com/demo/litway/http/HttpTest.java
@@ -31,6 +31,9 @@ public class HttpTest {
 
     private String ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
 
+    /**
+     * 测试http请求
+     */
     @Test
     public void testHttp() {
         HashMap<String, Object> query = new HashMap<>();

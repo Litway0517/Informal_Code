2e51507baae35dd0234387e7d3175f463944d1b4
Litway_ <xmw0517@gmail.com>
2023-06-28 20:59:35 +0800

    http  测试订单查询mapper

---
+++ http/src/main/java/com/demo/litway/mapper/OrderSaleMapper.java
@@ -0,0 +1,14 @@
+package com.demo.litway.mapper;
+
+import com.demo.litway.pojo.OrderSale;
+import com.github.yulichang.base.MPJBaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Litway
+ * @version 1.0
+ */
+@Mapper
+public interface OrderSaleMapper extends MPJBaseMapper<OrderSale> {
+
+}
